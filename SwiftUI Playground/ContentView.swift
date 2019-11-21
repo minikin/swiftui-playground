0760c535e410ec89193b71f6e264e5865cbd1539
minikin <djminikin@gmail.com>
2019-11-21 10:40:32 +0100

    Initial Commit

---
+++ SwiftUI Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Playground
+//
+//  Created by Developer on 21.11.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
