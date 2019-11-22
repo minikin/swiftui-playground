e9244677ba206890cf37e8b0d89f37d57fec794e
minikin <djminikin@gmail.com>
2019-11-22 15:43:12 +0100

    Temporarily disableable single picker

--- SwiftUI Playground/Modules/Multiple Select/MultipleSelect.swift
+++ SwiftUI Playground/Modules/Multiple Select/MultipleSelect.swift
@@ -10,42 +10,36 @@ import SwiftUI
 
 struct MultipleSelect: View {
     @State private var showMotorsSelection = false
-    @State private var selectedItem = Color.clear
     @ObservedObject var preferedMotors = PreferedMotors()
-    private let colors: [Color] = [.red, .green, .blue]
 
     var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Types").font(.caption)) {
-                    Button(action: {
-                        self.showMotorsSelection.toggle()
-                    }) {
-                        HStack {
-                            Text("Select multiply items").foregroundColor(Color.black)
-                            Spacer()
-                            Text("\(preferedMotors.motors.count)")
-                                .foregroundColor(Color(UIColor.systemGray))
-                                .font(.body)
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color(UIColor.systemGray4))
-                                .font(Font.body.weight(.medium))
-                        }
-                    }
-                    .sheet(isPresented: $showMotorsSelection) {
-                        MotorPickerView(self.preferedMotors)
-                    }
-                    Picker(selection: $selectedItem, label: Text("Select one item")) {
-                        ForEach(colors, id: \.self) { color in
-                            Text(color.description.capitalized)
-                                .padding()
-                                .background(color)
-                        }
-                    }
-                }
+        Form {
+            Section(header: Text("Types").font(.caption)) {
+                configureMotorPickerView()
+                //PickerView()
+            }
+            .navigationBarTitle("Pickers")
+        }
+    }
+
+    private func configureMotorPickerView() -> some View {
+        Button(action: {
+            self.showMotorsSelection.toggle()
+        }) {
+            HStack {
+                Text("Select multiply items").foregroundColor(Color.black)
+                Spacer()
+                Text("\(preferedMotors.motors.count)")
+                    .foregroundColor(Color(UIColor.systemGray))
+                    .font(.body)
+                Image(systemName: "chevron.right")
+                    .foregroundColor(Color(UIColor.systemGray4))
+                    .font(Font.body.weight(.medium))
             }
         }
-        .navigationBarTitle("Pickers")
+        .sheet(isPresented: $showMotorsSelection) {
+            MotorPickerView(self.preferedMotors)
+        }
     }
 }
 
---
+++ SwiftUI Playground/Modules/Multiple Select/PickerView.swift
@@ -0,0 +1,30 @@
+//
+//  PickerViewswift.swift
+//  SwiftUI Playground
+//
+//  Created by Developer on 22.11.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    @State private var selectedItem = Color.clear
+    private let colors: [Color] = [.red, .green, .blue]
+
+    var body: some View {
+        Picker(selection: $selectedItem, label: Text("Select one item")) {
+            ForEach(colors, id: \.self) { color in
+                Text(color.description.capitalized)
+                    .padding()
+                    .background(color)
+            }
+        }
+    }
+}
+
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
