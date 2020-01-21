b7aac5ee3d757447ab2771f72af5ad667a9e8f22
minikin <djminikin@gmail.com>
2020-01-21 11:42:30 +0100

    Add Key, update project

---
+++ SwiftUI Playground/Modules/PropertyWrapper/Key.swift
@@ -0,0 +1,23 @@
+//
+//  Key.swift
+//  SwiftUI Playground
+//
+//  Created by Developer on 21.01.20.
+//  Copyright © 2020 Sasha Prokhorenko. All rights reserved.
+//
+
+import Foundation
+
+struct Key: RawRepresentable {
+	let rawValue: String
+}
+
+extension Key: ExpressibleByStringLiteral {
+	init(stringLiteral: String) {
+		rawValue = stringLiteral
+	}
+}
+
+extension Key {
+	static let isFirstLaunch: Key = "isFirstLaunch"
+}
