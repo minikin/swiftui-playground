1ca87cd98d355ce9931ccdaca47110f7c83b92f8
minikin <djminikin@gmail.com>
2020-01-21 11:39:09 +0100

    Add PropertyListValue

---
+++ SwiftUI Playground/Modules/PropertyWrapper/UserDefault.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefault.swift
+//  SwiftUI Playground
+//
+//  Created by Sasha Prokhorenko on 21.01.20.
+//  Copyright © 2020 Sasha Prokhorenko. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T: PropertyListValue> {
+	let key: Key
+	
+	var wrappedValue: T? {
+			get { UserDefaults.standard.value(forKey: key.rawValue) as? T }
+			set { UserDefaults.standard.set(newValue, forKey: key.rawValue) }
+	}
+}
